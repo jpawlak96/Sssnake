030147e2bf20c2c7dc518d74d89c9b1fe6f2fe5f
Jakub Pawlak <jakub.pawlak@smartbear.com>
2022-11-09 07:58:20 +0100

    Move apple logic to game scene

--- src/entites/Apple.ts
+++ src/entites/Apple.ts
@@ -1,57 +1,10 @@
-import { Rectangle, Sprite, Texture } from 'pixi.js'
-import { HUD } from '../hud/HUD'
-import * as Utils from '../Utils'
-import { TILES_NUMBER, TILE_SIZE } from '../Constants'
-import { Snake } from './Snake'
+import { Sprite, Texture } from 'pixi.js'
+import { TILE_SIZE } from '../Constants'
 
 export class Apple extends Sprite {
-  snake: Snake
-  hud: HUD
-
-  constructor (snake: Snake, hud: HUD) {
+  constructor () {
     super(Texture.from('apple'))
     this.width = TILE_SIZE
     this.height = TILE_SIZE
-
-    this.snake = snake
-    this.hud = hud
-
-    this.changePosition()
-  }
-
-  update (): void {
-    if (this.snake.head.intersects(this.getBounds())) {
-      this.changePosition()
-      this.snake.isHungry = false
-      this.hud.increaseEatenApples()
-    }
-  }
-
-  draw (graphy: Graphics): void {
-    graphy.beginFill(0x00ff00)
-    graphy.drawRect(
-      this.position.x,
-      this.position.y,
-      this.position.width,
-      this.position.height
-    )
-    graphy.endFill()
-  }
-
-  private changePosition (): void {
-    while (true) {
-      const newX = Utils.getRandomInt(TILES_NUMBER - 1) * this.width
-      const newY = Utils.getRandomInt(TILES_NUMBER - 1) * this.height
-
-      const newPosition = new Rectangle(newX, newY, TILE_SIZE, TILE_SIZE)
-
-      const colisionSnakeParts = this.snake.parts.filter((part) =>
-        part.intersects(newPosition)
-      )
-      if (colisionSnakeParts.length === 0) {
-        this.position.set(newX, newY)
-        return
-      }
-    }
   }
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,9 +1,11 @@
-import { TICK_PER_SEC } from '../Constants'
+import { TICK_PER_SEC, TILES_NUMBER, TILE_SIZE } from '../Constants'
 import { State } from '../enums/State'
 import { HUD } from '../hud/HUD'
 import { AbstractContainer } from './AbstractScene'
 import { Apple } from '../entites/Apple'
 import { Snake } from '../entites/Snake'
+import * as Utils from '../Utils'
+import { IPointData, Rectangle } from 'pixi.js'
 
 export class GameScene extends AbstractContainer {
   state: State
@@ -20,7 +22,9 @@ export class GameScene extends AbstractContainer {
 
     this.hud = new HUD(this.bounds.width, this.bounds.height)
     this.snake = new Snake()
-    this.apple = new Apple(this.snake, this.hud)
+    this.apple = new Apple()
+    const newApplePosition = this.generateApplePosition()
+    this.apple.position = newApplePosition
 
     this.addChild(this.apple)
     this.addChild(this.snake)
@@ -33,7 +37,6 @@ export class GameScene extends AbstractContainer {
       if (this.deltaCounter > 1 / TICK_PER_SEC) {
         this.deltaCounter -= 1 / TICK_PER_SEC
         this.snake.update()
-        this.apple.update()
         this.updateGameState()
       }
     }
@@ -41,17 +44,42 @@ export class GameScene extends AbstractContainer {
   }
 
   private updateGameState (): void {
-    if (this.isColision()) {
+    if (this.isSnakeCollides()) {
       this.state = State.GameOver
+    } else if (this.isSnakeEats()) {
+      const newApplePosition = this.generateApplePosition()
+      this.apple.position = newApplePosition
+      this.snake.isHungry = false
+      this.hud.increaseEatenApples()
     }
   }
 
-  private isColision (): boolean {
+  private isSnakeCollides (): boolean {
     return (
       !this.bounds.intersects(this.snake.head) ||
       this.snake.parts.filter((part) =>
         part !== this.snake.head &&
         part.intersects(this.snake.head)).length !== 0
     )
+  }
+
+  private isSnakeEats (): boolean {
+    return this.snake.head.intersects(this.apple.getBounds())
+  }
+
+  private generateApplePosition (): IPointData {
+    while (true) {
+      const newX = Utils.getRandomInt(TILES_NUMBER - 1) * TILE_SIZE
+      const newY = Utils.getRandomInt(TILES_NUMBER - 1) * TILE_SIZE
+
+      const newPosition = new Rectangle(newX, newY, TILE_SIZE, TILE_SIZE)
+
+      const colisionSnakeParts = this.snake.parts.filter((part) =>
+        part.intersects(newPosition)
+      )
+      if (colisionSnakeParts.length === 0) {
+        return { x: newX, y: newY }
+      }
+    }
   }
 }
